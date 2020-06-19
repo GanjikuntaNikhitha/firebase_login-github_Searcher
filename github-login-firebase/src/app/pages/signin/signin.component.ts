5e9b990b3da3c305a818d4fd9af07feb41f4f1e0
GanjikuntaNikhitha <nikhitha.ganjigunta62@gmail.com>
2020-06-19 17:54:00 +0530

    signin component

--- github-login-firebase/src/app/pages/signin/signin.component.ts
+++ github-login-firebase/src/app/pages/signin/signin.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from './../../services/auth.service';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor( private auth: AuthService,
+    private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
-
+  onSubmit(f: NgForm) {
+    const { email, password } = f.form.value;
+    //TODO: do your checking here
+    this.auth
+      .signIn(email, password)
+      .then((res) => {
+        this.router.navigateByUrl('/');
+        this.toastr.success('Signup Success');
+      })
+      .catch((err) => {
+        console.log(err.message);
+        this.toastr.error('Singup failed');
+      });
+  }
 }
