0a7622d25505131b559003602f36f1a6b20ae156
GanjikuntaNikhitha <nikhitha.ganjigunta62@gmail.com>
2020-06-19 16:55:33 +0530

    home component,gitrepo

--- github-login-firebase/src/app/pages/home/home.component.ts
+++ github-login-firebase/src/app/pages/home/home.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,ChangeDetectorRef  } from '@angular/core';
+import { GithubService } from './../../services/github.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  user = null;
+  userName: string;
+  error = null;
 
-  constructor() { }
+  constructor(  private ref: ChangeDetectorRef,
+    private githubService: GithubService) { }
 
   ngOnInit(): void {
   }
-
+  handleFind() {
+    this.githubService.getUserDetails(this.userName).subscribe(
+      (user) => {
+        this.user = user;
+        this.error = null;
+        // repeating again and again so we are using below code
+        this.ref.detectChanges();
+      },
+      (err) => {
+        this.user = null;
+        this.error = 'User not found';
+        this.ref.detectChanges();
+      }
+    );
+  }
 }
